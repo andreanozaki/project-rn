d9fcd02c68e547f6236a0bbb3637938bf5eec4c1
andreanozaki <nozakiandrea@gmail.com>
2024-10-30 14:25:49 -0300

    feat:configurando teste de integracao com bd

--- js/db.test.js
+++ js/db.test.js
@@ -1,4 +1,8 @@
 // db.test.js
+//Este arquivo de teste usa a conexão configurada no ambiente de
+// teste para verificar se a conexão com o banco de dados ocorre sem erros.
+//Testes de conexão: db.test.js se conecta ao banco usando a configuração .env.test.
+
 const connection = require('./db');
 
 test('Conexão com o banco de dados deve ser bem-sucedida', (done) => {
